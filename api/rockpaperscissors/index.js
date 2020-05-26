bb86a061a06ff4e84135a93cbab3b243fda783fa
stuartleaver <stuartleaveruk@gmail.com>
2020-05-26 20:10:41 +0100

    Fix max random number
    Changed the random number so that it only generates 0-2 as sometimes it was generating 3 as well.
    
    #5

--- api/rockpaperscissors/index.js
+++ api/rockpaperscissors/index.js
@@ -1,8 +1,8 @@
-module.exports = async function (context, req) {
+module.exports = async function(context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: Math.floor(Math.random() * 3) + 1
+        body: Math.floor(Math.random() * 3)
     };
 };
